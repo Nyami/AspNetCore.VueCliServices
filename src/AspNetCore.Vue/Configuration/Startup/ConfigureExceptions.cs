1bd10457d69adaade2073074e6a9c28a3a086ae8
UseMuse <UseMuse@mail.ru>
2019-10-15 13:24:25 +0300

    replacing deprecated IHostingEnvironment with IWebHostEnvironment

--- src/AspNetCore.Vue/Configuration/Startup/ConfigureExceptions.cs
+++ src/AspNetCore.Vue/Configuration/Startup/ConfigureExceptions.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace Nyami.AspNetCore.VueApp.Configuration.Startup
 {
     public static partial class ConfigurationExtensions
     {
-        public static IApplicationBuilder ConfigureExceptions(this IApplicationBuilder app, IHostingEnvironment env)
+        public static IApplicationBuilder ConfigureExceptions(this IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
--- src/AspNetCore.Vue/Startup.cs
+++ src/AspNetCore.Vue/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Nyami.AspNetCore.VueCliServices;
 
 namespace Nyami.AspNetCore.VueApp
